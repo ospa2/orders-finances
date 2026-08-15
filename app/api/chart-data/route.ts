615d506c4b3fa87217c844839e545458f1db3268
Seraphim <sserka1q@gmail.com>
2026-08-15 17:57:41 +0500

    хуй пизда

--- app/api/chart-data/route.ts
+++ app/api/chart-data/route.ts
@@ -16,6 +16,8 @@ export async function GET() {
     supabase.from("orders").select("*").range(PAGE_SIZE * 4, PAGE_SIZE * 5 - 1),
     supabase.from("orders").select("*").range(PAGE_SIZE * 5, PAGE_SIZE * 6 - 1),
     supabase.from("orders").select("*").range(PAGE_SIZE * 6, PAGE_SIZE * 7 - 1),
+    supabase.from("orders").select("*").range(PAGE_SIZE * 7, PAGE_SIZE * 8 - 1),
+
   ]);
 
   const orders: Order[] = [];
